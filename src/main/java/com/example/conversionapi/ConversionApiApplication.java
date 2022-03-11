d6e330f4f84c93cd7421d09b97942d4aaea10925
Rado <radoslavcv91@gmail.com>
2022-03-11 11:41:45 +0100

    init

--- src/main/java/com/example/conversionapi/ConversionApiApplication.java
+++ src/main/java/com/example/conversionapi/ConversionApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConversionApiApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(ConversionApiApplication.class, args);
     }
