4032f9af8c759d6f223e4de4c595dda8cd62c3f5
mattesho <esho_matti@hotmail.com>
2022-03-11 13:56:20 +0100

    added feature Convert Fahrenheit to Celsius

---
+++ src/main/java/com/example/conversionapi/repository/ConvertFahrenheitRepo.java
@@ -0,0 +1,29 @@
+package com.example.conversionapi.repository;
+
+import com.example.conversionapi.model.UnitConversionRequestModel;
+import com.example.conversionapi.strategy.ConversionStrategy;
+
+public class ConvertFahrenheitRepo implements ConversionStrategy {
+
+    @Override
+    public boolean requestIsValid(UnitConversionRequestModel request) {
+        if (toTypeIsValid(request.getToType())) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean toTypeIsValid(String toType) {
+        if (toType.equals("C") || toType.equals("c")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public float getConvertedValue(float fromValue) {
+        return (fromValue - 32) * 5 / 9;
+    }
+}
