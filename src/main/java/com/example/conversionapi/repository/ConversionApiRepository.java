5303f183c3b2da05a5b5c7e9517edefbd1d182ff
mattesho <esho_matti@hotmail.com>
2022-03-11 14:19:24 +0100

    added logic to main repo

--- src/main/java/com/example/conversionapi/repository/ConversionApiRepository.java
+++ src/main/java/com/example/conversionapi/repository/ConversionApiRepository.java
@@ -3,13 +3,11 @@ package com.example.conversionapi.repository;
 
 
 import com.example.conversionapi.model.ConversionRestResponseModel;
+import com.example.conversionapi.model.InvalidRequestResponseModel;
 import com.example.conversionapi.model.UnitConversionRequestModel;
 import com.example.conversionapi.strategy.ConversionStrategy;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 @Repository
 public class ConversionApiRepository {
@@ -21,8 +19,39 @@ public class ConversionApiRepository {
 
         ConversionStrategy converter;
 
-        return null;
+        switch (request.getFromType()) {
+            case "g":
+            case "G":
+                converter = new ConvertGramRepo();
+                break;
+            case "kg":
+            case "KG":
+            case "Kg":
+            case "kG":
+                converter = new ConvertKilogramRepo();
+                break;
+            case "c":
+            case "C":
+                converter = new ConvertCelsiusRepo();
+                break;
+            case "f":
+            case "F":
+                converter = new ConvertFahrenheitRepo();
+                break;
+            default:
+                return new InvalidRequestResponseModel(request);
+        }
 
+        if (converter.requestIsValid(request)) {
+            float result = converter.getConvertedValue(request.getFromValue());
+            ConversionRestResponseModel response = new ConversionRestResponseModel(request);
+            response.setValid(true);
+            response.setResult(result);
+            return response;
+
+        } else {
+            return new InvalidRequestResponseModel(request);
+        }
     }
 
 }
